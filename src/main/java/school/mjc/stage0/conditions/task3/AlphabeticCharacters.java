6dc717b190730959079c5f0c64281da0194c54a1
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-03 09:35:58 +0500

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -4,8 +4,7 @@ public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
         if(character=='a' || character=='o' || character=='i' || character=='e' || character=='u'){
             System.out.println("Vowel");
-         else if(character>='a' && character<='z')
-         {
+        } else if(character>='a' && character<='z') {
              System.out.println("Consonant");
          } else {
              System.out.println("wrong alphabet!");
